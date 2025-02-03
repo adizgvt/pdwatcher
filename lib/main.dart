bf2da1ce66c69677195220d5d319b9906d3bf1c4
adib <adibf72@gmail.com>
2025-02-03 18:05:33 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:launch_at_startup/launch_at_startup.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pdwatcher/chunker.dart';
 import 'dart:io';
 import 'dart:typed_data';
@@ -35,77 +37,18 @@ void main() async {
   windowManager.setMaximumSize(const Size(800, 600));
   windowManager.setSkipTaskbar(false);
 
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+  launchAtStartup.setup(
+    appName: packageInfo.appName,
+    appPath: Platform.resolvedExecutable,
+    // Set packageName parameter to support MSIX.
+    packageName: 'com.example.pdwatcher',
+  );
+
+  await launchAtStartup.enable();
+
   runApp(MyApp());
-
-  return;
-
-  String path = 'C:\\Users\\pd\\Desktop\\watch';
-  int chunkSize = 1024 * 4 * 1024; //4MB
-
-  var filex = File(path + '\\contoh.docx');
-  var raf = filex.openSync(mode: FileMode.read);
-
-  // Lock the file asynchronously with an exclusive lock
-  raf.lockSync(FileLock.exclusive);
-
-  await Future.delayed(Duration(seconds: 300));
-
-  return;
-
-
-
-   await Chunker.chunkFile('C:\\Users\\pd\\Desktop\\watch\\largedocumentcopy.docx', chunkSize);
-   print('----');
-  // await Chunker.chunkFile('C:\\Users\\pd\\Desktop\\watch\\largedocumentcopy.docx', chunkSize);
-
-  return;
-
-  String filePath = 'C:\\Users\\pd\\Desktop\\watch\\SaleData.xlsx';
-  String filePath2 = 'C:\\Users\\pd\\Desktop\\watch\\SaleDataMod.xlsx';
-
-  // Read the file as bytes
-  File file = File(filePath);
-  File file2 = File(filePath2);
-  Uint8List fileData = await file.readAsBytes();
-  Uint8List fileData2 = await file2.readAsBytes();
-
-  // Initialize chunker
-  ContentDefinedChunker chunker = ContentDefinedChunker();
-
-  // Get chunks
-  List<Uint8List> chunks = chunker.chunkData(fileData);
-  List<Uint8List> chunks2 = chunker.chunkData(fileData2);
-
-  // Calculate MD5 for each chunk
-  for (int i = 0; i < chunks.length; i++) {
-    Digest md5Hash = md5.convert(chunks[i]);
-    Digest md5Hash2 = md5.convert(chunks2[i]);
-    print('$md5Hash : $md5Hash2');
-  }
-
-  //return;
-
-  //DIRECTORY WATCHER
-  // Watcher watcher = DirectoryWatcher(path);
-  // watcher.events.listen((event){
-  //  print(DateTime.now());
-  //  print(event.type);
-  //  print(event.path);
-  // });
-
-  //CHUNKER
-
-  //HASHER
-
-  //MERGER
-
-  //QUEUER
-
-  //SYNCER
-
-  //LOCKER
-
-
 }
 
 class Dummy extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import connectivity_plus_macos
 import custom_platform_device_id
+import package_info_plus
 import path_provider_foundation
 import platform_device_id_macos
 import shared_preferences_foundation
@@ -16,6 +17,7 @@ import window_manager
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
